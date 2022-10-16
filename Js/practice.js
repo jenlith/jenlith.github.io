a1664bb352ff7a38a66a8fc0dac24942c0aecf63
Jen <87286211+jenlith@users.noreply.github.com>
2022-10-15 21:29:31 -0400

    jQuery?

--- Js/practice.js
+++ Js/practice.js
@@ -1,7 +1,13 @@
-function helloWorld() {
+function firstTry() {
     return "This is javascript from a js file";
 }
 
 function btnFunc() {
     alert("hi");
+}
+
+function aJQuery () {
+    //test3
+    $('#test3').load('navbar.html');
+    $('#test4').load('navbar.html');
 }
