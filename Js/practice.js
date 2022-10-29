7a14c3c5289b7c3659d8b7ac97bb93922042e85a
Jen <87286211+jenlith@users.noreply.github.com>
2022-10-29 15:00:58 -0400

    .

--- Js/practice.js
+++ Js/practice.js
@@ -3,6 +3,6 @@ function btnFunc() {
 }
 
 function aJQuery () {
-    $('#display_aJQuery').text("<p>Hello World</p>"); //prints out the whole thing, doesn't html format
+    $('#display_aJQuery').text("Hello World"); //prints out the whole thing, doesn't html format
     $('#navbar').load('navbar.html'); //not yet working
 }
